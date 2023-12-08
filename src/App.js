a3a296f05dbdf312b8ff9b5b0272cc589c0b39cd
loretari <loretari@gmail.com>
2023-12-08 12:00:40 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import "./App.css";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
+import User from "./pages/user/User";
+import UserList from "./pages/userList/UserList";
+import NewUser from "./pages/newUser/NewUser";
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
+import NewProduct from "./pages/newProduct/NewProduct";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+     <Topbar/>
+
+       <div className= "container">
+
+           <Sidebar/>
+               <Routes>
+           <Route index path= "/" element={<Home/>}/>
+           <Route path= "/users" element={<UserList/>}/>
+           <Route path= "/user/:userId" element={<User/>}/>
+           <Route path= "/newUser" element={<NewUser/>}/>
+           <Route path= "/products" element={<ProductList/>} />
+           <Route path= "/product/:ID" element={<Product/>}/>
+           <Route path= "/newproduct" element={<NewProduct/>} />
+           </Routes>
+       </div>
+
+        </BrowserRouter>
+  );
+}
+
+export default App;
